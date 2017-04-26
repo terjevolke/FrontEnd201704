9447fd3403e95dc69065187d97e18c19166a3872
TuuliP <tuuler@gmail.com>
2017-04-26 14:52:38 +0300

    no message

---
+++ Tuuli/HTMLkood/scripts/script.js
@@ -0,0 +1,9 @@
+"use strict";
+var muutuja = 5;
+var s6na = "Tere päevast";
+var massiiv = ["Tere", "Hi", "Hommikust"];
+var objekt2 = {"nimi": "Kalle", 
+                "vanus": 6 };
+var objekt = null; 
+alert(objekt2["nimi"]);
+
