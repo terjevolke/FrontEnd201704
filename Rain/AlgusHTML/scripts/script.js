f878d57929fce1b2dc790b8431541d48675e153d
rainsirendi <rainsirendi@gmail.com>
2017-04-26 14:41:52 +0300

    Javascripti algus

--- Rain/AlgusHTML/scripts/script.js
+++ Rain/AlgusHTML/scripts/script.js
@@ -1,3 +1,32 @@
+"use strict";
 var muutuja = 5;
 var s6ne = "Tere Päevast";
-alert(s6ne);
+var massiiv = ["Tere", "Hi", "Hommikust" ];
+var objekt2 = {"nimi": "Kalle", "vanus": 6};
+var objekt = null;
+var muutuja2; //antakse väärtuseta muutuja
+//alert(objekt2["nimi"]);
+//alert(massiiv[0]);
+//alert(s6ne);
+
+//if ja switch
+// != ei ole võrdne; && ja; === on võrdne; == võrdväärne; !== ei tohi olla võrdne; <=; >=;
+// || või; ! ei;
+if(typeof objekt2["nimi"] === "string"){
+        alert(objekt2["nimi"]);
+    }else if(typeof objekt2["vanus"] === "number"){
+        alert("arv");
+    }else{}
+
+switch (muutuja) {
+    case 0:
+        alert("arv0");
+        break;
+        case 5:
+
+    alert("arv5");
+        break;
+
+    default:
+        break;
+}
