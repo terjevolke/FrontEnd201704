abb531d321f1c9698a16e9eb815d114b50c293a7
rainsirendi <rainsirendi@gmail.com>
2017-04-27 10:20:35 +0300

    Tsükkel ja teisendamine

--- Rain/AlgusHTML/scripts/script.js
+++ Rain/AlgusHTML/scripts/script.js
@@ -30,7 +30,7 @@ switch (muutuja) {
     default:
         break;
 }
-muutja = 6 + 4;
+muutuja = 6 + 4;
 // +;-;*,/ -võimalikud tehted
 muutuja = 5 % 1;  //jääk
 muutuja = muutuja + 1;
@@ -40,4 +40,31 @@ muutuja--; --muutuja; //vähendab ühe võrra
 muutuja += 2; // +=; -=; *=; /=
 
 
+var pikkus = massiiv.lenght;
+for(var i = 0; i < pikkus; ++i){
+    massiiv[i] += " ";
+} // pikkus on kolm, kuna massiivis on kolm elementi, see käiakse kolm korda läbi ja igale massiivi elemendiele lisatakse tühik
+//alert(massiiv);
+console.log(massiiv);  //teeb töö ära, kuid kuvab ainult INSPECT ELEMENT->Console aknas.
+
+
+var text ="";
+for(var x in objekt2){
+    text += (objekt2[x] + " ");
+} // see tsükkel käib läbi iga elemendi massiivis, ja kuvab väärtuse (kalle 6)
+console.log(text);
+
+
+var k = 0;
+var summa = 0;
+while (k<10) {
+    if (k < 0)
+        break;
+        //continue; katkestab korraks???
+    summa += k;
+    ++k;
+}
+console.log(summa)
+
+//do{}while(true); //haruldane
 
