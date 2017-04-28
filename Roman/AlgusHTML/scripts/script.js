1f7085e494207aff5a169229661307b2de025fbb
Roman Gorislavski <roman.goris@hotmail.com>
2017-04-28 10:20:43 +0300

    Prototypal

--- Roman/AlgusHTML/scripts/script.js
+++ Roman/AlgusHTML/scripts/script.js
@@ -241,7 +241,7 @@ tyhiFunksioon();
     minuMoodul.muudaNimi('Madis');
     minuMoodul.ytleNimi();
 })();
-(function(){
+/*(function(){
     function p2randab(laps, vanem){
         laps.vanem_ = vanem;
         laps.prototype = Object.create(
@@ -277,7 +277,6 @@ tyhiFunksioon();
     //Pärandamine
     var Lind = function(nimetus, h22litsus, kasLendab){
         Lind.vanem_.call(this, nimetus, h22litsus);
-        //Lind.vanem_.call(this, h22litsus);
         this.kasLendab = kasLendab;
     };
     p2randab(Lind, Loom);
@@ -292,6 +291,51 @@ tyhiFunksioon();
         console.log('See lind on ' + this.nimetus);
     }
     var sinuKana = new Lind('kana', 'pak-pak', false);
+    sinuKana.nimeta();
+    sinuKana.lenda();
+})();*/
+(function(){
+    //prototypal
+    var objekt = {
+        klass: 'objekt',
+        loo: function(v22rtused){
+            var instant = Object.create(this);
+            Object.keys(v22rtused).forEach(
+                function(v6ti){
+                    instant[v6ti] = v22rtused[v6ti];
+                }
+            );
+            return instant;
+        }
+    };
+    var loom = objekt.loo({
+        klass: 'loom',
+        klassifitseeri: function(){
+            console.log(this.klass);
+        },
+        nimeta: function(){
+            console.log('See loom on ' + this.nimetus);
+        }  
+    });
+    var lind = loom.loo({
+        klass: 'lind',
+        nimeta: function(){
+            console.log('See lind on ' + this.nimetus);
+        },
+        lenda: function(){
+           if(this.kasLendab){
+                console.log('Ma lendan');
+            }else {
+                console.log('Ära õrrita');
+            } 
+        }
+    });
+
+    var sinuKana = lind.loo({
+        nimetus: 'kana',
+        kaslendab: false
+    });
+    sinuKana.klassifitseeri();
     sinuKana.nimeta();
     sinuKana.lenda();
 })();
