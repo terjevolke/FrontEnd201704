8dcd9c9b369453bdd1fdb8b8875afffeff9e06be
TuuliP <tuuler@gmail.com>
2017-05-02 09:43:05 +0300

    1.05.seis_pooleli kodutöö

--- Tuuli/HTMLkood/scripts/script.js
+++ Tuuli/HTMLkood/scripts/script.js
@@ -1,20 +1,6 @@
 "use strict";
 
-<body><script type="text/javascript">
-var kuupaev = new Date();
-var paev = kuupaev.getDate();
-var kuu = kuupaev.getMonth();
-var aasta = kuupaev.getFullYear();
-document.write("<b>" + paev + "/" + (kuu + 1) + "/" + aasta + "</b>");
-</script>
 
-(function){
-    document;
-    document.getElementsByTagName(´nav`) [0];
-    /*saad kätte kõik elemendid mis tag-ga, näiteks navigatsioon (mitmuses)*/
-}
-function domLoaded(){
-        alert(`Tere`);
-        }
-    document.addEventListener(`DOMContentLoaded, domLoaded)
-    
+
+
+
